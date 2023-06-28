fa5930f42f4a99ac16cd6375cf800ce9ca628ac0
João Paulo <72082760+joaopaludo@users.noreply.github.com>
2023-06-27 21:46:35 -0300

    Script de inserção de dados

---
+++ scripts/script_insercao_railwise.sql
@@ -0,0 +1,38 @@
+-- inserção de clientes
+INSERT INTO cliente VALUES (default, 'Pedro', '44515943081', '26/06/2002', 1);
+INSERT INTO cliente VALUES (default, 'Maria', '12345678901', '10/01/2023', 1);
+INSERT INTO cliente VALUES (default, 'João', '98765432109', '05/03/2023', 2);
+INSERT INTO cliente VALUES (default, 'Ana', '45678912345', '20/02/2022', 3);
+INSERT INTO cliente VALUES (default, 'Carlos', '78901234567', '08/04/2023', 1);
+INSERT INTO cliente VALUES (default, 'Laura', '89012345678', '11/02/2023', 1);
+INSERT INTO cliente VALUES (default, 'Rafaela', '43210987654', '28/04/2022', 2);
+INSERT INTO cliente VALUES (default, 'Gustavo', '34567891234', '07/03/2023', 3);
+INSERT INTO cliente VALUES (default, 'Matheus', '90123456789', '19/01/2021', 2);
+INSERT INTO cliente VALUES (default, 'Vitória', '32109876543', '31/03/2023', 3);
+INSERT INTO cliente VALUES (default, 'Frigorífico Périco', '67166426000190', '20/01/2022', 1);
+INSERT INTO cliente VALUES (default, 'Supermercado ABC', '12345678901234', '15/06/2023', 2);
+INSERT INTO cliente VALUES (default, 'Restaurante Delícia', '11111111111111', '01/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Loja de Roupas Fashion', '22222222222222', '02/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Salão de Beleza Encanto', '55555555555555', '05/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Oficina Mecânica Veloz', '66666666666666', '06/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Academia Corpo em Movimento', '77777777777777', '07/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Farmácia Bem-Estar', '88888888888888', '08/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Pizzaria Sabor e Arte', '99999999999999', '09/07/2023', 1);
+INSERT INTO cliente VALUES (default, 'Cafeteria Aconchego', '10101010101010', '10/07/2023', 1);
+
+
+-- inserção de estações
+INSERT INTO estacao VALUES (default, 'Jardim Colonial', 'São Paulo', 'SP', 'Av. Ragueb Chohfi, 1400, São Mateus', '08:00', '20:00');
+INSERT INTO estacao VALUES (default, 'Estação Central', 'Rio de Janeiro', 'RJ', 'Praça Cristiano Otoni, s/n, Central do Brasil', '07:00', '22:00');
+INSERT INTO estacao VALUES (default, 'Estação Bela Vista', 'São Paulo', 'SP', 'Rua Santo Antônio, 100, Bela Vista', '06:30', '21:30');
+INSERT INTO estacao VALUES (default, 'Estação da Lapa', 'Salvador', 'BA', 'Praça Visconde de Cayru, s/n, Comércio', '05:45', '20:30');
+INSERT INTO estacao VALUES (default, 'Estação Barra Funda', 'São Paulo', 'SP', 'Rua Tagipuru, 641, Barra Funda', '05:00', '23:00');
+INSERT INTO estacao VALUES (default, 'Estação da Luz', 'São Paulo', 'SP', 'Praça da Luz, s/n, Luz', '04:30', '00:30');
+INSERT INTO estacao VALUES (default, 'Estação Antônio Bezerra', 'Fortaleza', 'CE', 'Av. Cel. Carvalho, 608, Antônio Bezerra', '05:30', '22:30');
+INSERT INTO estacao VALUES (default, 'Estação Jardim Oceânico', 'Rio de Janeiro', 'RJ', 'Av. Ministro Ivan Lins, 300, Barra da Tijuca', '06:00', '21:00');
+INSERT INTO estacao VALUES (default, 'Estação Ana Rosa', 'São Paulo', 'SP', 'Av. Conselheiro Rodrigues Alves, 184, Paraíso', '05:30', '00:00');
+INSERT INTO estacao VALUES (default, 'Estação da Candelária', 'Rio de Janeiro', 'RJ', 'Praça Pio X, s/n, Centro', '07:00', '20:00');
+INSERT INTO estacao VALUES (default, 'Estação Campo Grande', 'Rio de Janeiro', 'RJ', 'Rua Cândido Benício, 2475, Campo Grande', '06:30', '21:30');
+
+
+-- inserção de locomotivas
