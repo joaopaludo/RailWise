163fae66c6b5d63317ff603adf6b1e7e02f20fa0
joaopaludo <joaopgpaludo@gmail.com>
2023-11-11 14:25:00 -0300

    Gerado projeto Spring - RailWise Java

---
+++ railwise_java/src/main/java/br/edu/unoesc/railwise/RailwiseApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.railwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwiseApplication.class, args);
+	}
+
+}
---
+++ railwise_java/src/test/java/br/edu/unoesc/railwise/RailwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc.railwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
